e4d8e79a986fb3b9c488ccc68f592b5b24ab53f5
Hernan Medina <alexyz656@gmail.com>
2018-08-26 19:11:00 -0700

    This is not fear of commitment

---
+++ TankBattle/Source/TankBattle/Private/TankAIController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "Engine/World.h"
+
+void ATankAIController::BeginPlay() {
+	Super::BeginPlay();
+
+	auto controlledTank = getControlledTank();
+	auto playerTank = getPlayerTank();
+	if (!controlledTank || !playerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("mijo la estas cagando"), );
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("I am controlling %s"), *controlledTank->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("And player i %s"), *playerTank->GetName());
+	}
+}
+
+ATank * ATankAIController::getPlayerTank() const
+{
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
+
+ATank* ATankAIController::getControlledTank() const {
+	return Cast<ATank>(GetPawn());
+}
---
+++ TankBattle/Source/TankBattle/Private/TankPlayerController.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	auto controlledTank = getControlledTank();
+	if (!controlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("mijo la estas cagando"), );
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("I am ihjuegrgiyufdeswygudfsygusfde %s"), *controlledTank->GetName());
+	}
+}
+
+ATank* ATankPlayerController::getControlledTank() const {
+	return Cast<ATank>(GetPawn());
+}
+
+
+
+
